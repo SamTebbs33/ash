0aae9a8d96652f6dfd4e955ab9dcad1f5401ff48
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-05-29 15:15:13 +0100

    java.lang.String and java.lang.Object are not imported by default

--- src/ashc/main/AshMain.java
+++ src/ashc/main/AshMain.java
@@ -2,6 +2,8 @@ package ashc.main;
 
 import java.io.*;
 
+import ashc.load.*;
+
 /**
  * Grammar
  *
@@ -10,6 +12,8 @@ import java.io.*;
 public class AshMain {
 
     public static void main(final String[] args) throws FileNotFoundException, IOException {
+	TypeImporter.loadClass("java.lang.String");
+	TypeImporter.loadClass("java.lang.Object");
 	final AshCompiler compiler = new AshCompiler(args[0]);
 	compiler.parse();
 	compiler.preAnalyse();
