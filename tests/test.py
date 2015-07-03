3646838a7514e0baa0cdb86eeb76f0c3ffd4d8d8
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-07-03 20:28:40 +0100

    Added None checking in test.py

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,6 @@ import yaml
 import subprocess
 import sys
 
-file = open("tests.yml", "r")
-yml = yaml.load(file)
 result_list = []
 failed_tests = 0
 num_tests = 0
@@ -21,24 +19,29 @@ def get_symbol(test_pass):
 def print_tests():
 	print "\tResults:"
 	print "\t\tExpect\tPass\tName"
-	for test in result_list:
-		test_name = test[0]
-		test_pass = test[1]
-		test_status_code = test[2]
-		if test_pass != test_status_code:
-			colour = "\x1B[31m"
-		else:
-			colour = "\x1B[32m"
-		print colour+"\t\t["+get_symbol(test_pass)+"]\t["+get_symbol(test_status_code)+"]\t" + test_name + "\x1B[00m"		
+	if result_list is not None:
+		for test in result_list:
+			test_name = test[0]
+			test_pass = test[1]
+			test_status_code = test[2]
+			if test_pass != test_status_code:
+				colour = "\x1B[31m"
+			else:
+				colour = "\x1B[32m"
+			print colour+"\t\t["+get_symbol(test_pass)+"]\t["+get_symbol(test_status_code)+"]\t" + test_name + "\x1B[00m"		
 
-for test in yml:
-	test_name = test
-	test_pass = 0 if yml[test]["pass"] else 1
-	status_code = do_test(test_name)
-	if status_code != test_pass:
-		failed_tests += 1
-	num_tests += 1
-	result_list.append((test_name, test_pass, status_code))
+file = open("tests.yml", "r")
+yml = yaml.load(file)
+
+if yml is not None:
+	for test in yml:
+		test_name = test
+		test_pass = 0 if yml[test]["pass"] else 1
+		status_code = do_test(test_name)
+		if status_code != test_pass:
+			failed_tests += 1
+		num_tests += 1
+		result_list.append((test_name, test_pass, status_code))
 	
 print_tests()
 print "\n\t" + `failed_tests` + "/" + `num_tests` + " tests failed\n"
