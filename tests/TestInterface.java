beb816b6aafcc4de47d6e7b7033af5ef20fbd209
SamTebbs33 <samuel.tebbs@yahoo.com>
2015-11-11 21:39:26 +0000

    Disabled lambda expression test

--- tests/TestInterface.java
+++
@@ -1,5 +0,0 @@
-public interface TestInterface {
-
-    public boolean test(int a, int b);
-
-}
--- tests/test.py
+++ tests/test.py
@@ -7,10 +7,11 @@ failed_tests = 0
 num_tests = 0
 
 def do_test(name, path, run):
-	print "\n\t\x1B[34m Compiling: " + name + "\x1B[00m"
+	print "\n\t\x1B[34m Testing: " + name + "\x1B[00m"
+	print "\n\t\t\x1B[34m Compiling:\x1B[00m"
 	status = subprocess.call(["java", "-cp", "../build:../lib/asm-5.0.4.jar:../lib/asm-tree-5.0.4.jar:../lib/asm-analysis-5.0.4.jar:../lib/asm-commons-5.0.4.jar:../lib/asm-util-5.0.4.jar", "ashc/main/AshMain", path+".ash", "-v", "-o", "../classes"])
 	if run and status == 0:
-		print "\n\t\x1B[34m Running: " + name + "\x1B[00m"
+		print "\n\t\t\x1B[34m Running:\x1B[00m"
 		status = subprocess.call(["java", "-cp", "../classes", path])
 	return status
 
